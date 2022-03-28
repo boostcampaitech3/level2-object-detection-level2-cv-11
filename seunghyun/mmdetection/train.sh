af9e5390b77e86ce2d658d69cd4869c43601dda6
sseunghyuns <allindata96@gmail.com>
2022-03-28 12:14:04 +0000

    Update train.sh

--- seunghyun/mmdetection/train.sh
+++ seunghyun/mmdetection/train.sh
@@ -1,4 +1,4 @@
 python tools/train.py \
- /opt/ml/detection/SEUNGHYUN_WORKSPACE/my_mmdetection/configs/collections/cascade_rcnn_swin-s-p4-w7_fpn_fp16_ms-crop-3x_coco.py \
- --work-dir /opt/ml/detection/SEUNGHYUN_WORKSPACE/my_mmdetection/work_dirs/cascade_rcnn_swin-s \
+ configs/collections/cascade_rcnn_swin-s-p4-w7_fpn_fp16_ms-crop-3x_coco.py \
+ --work-dir work_dirs/cascade_rcnn_swin-s \
 #  --resume-from /opt/ml/detection/SEUNGHYUN_WORKSPACE/my_mmdetection/work_dirs/cascade_rcnn_swin-s-p4-w7_fpn_fp16_ms-crop-3x_coco/epoch_24.pth
