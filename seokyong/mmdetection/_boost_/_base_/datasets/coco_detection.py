5304f82ca1a504167d4444fef83c575a78771645
HeoSeokYong <hsdragoner@gmail.com>
2022-03-30 10:46:08 +0900

    Upload SeokYong mmdetection configs

---
+++ seokyong/mmdetection/_boost_/_base_/datasets/coco_detection.py
@@ -0,0 +1,57 @@
+# dataset settings
+dataset_type = 'CocoDataset'
+data_root = '/opt/ml/detection/dataset/'
+classes = ("General trash", "Paper", "Paper pack", "Metal", "Glass", 
+           "Plastic", "Styrofoam", "Plastic bag", "Battery", "Clothing")
+img_norm_cfg = dict(
+    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+
+img_scale = (1024, 1024)
+
+train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='LoadAnnotations', with_bbox=True),
+    dict(type='Resize', img_scale=(1024, 1024), keep_ratio=True),
+    dict(type='RandomFlip', flip_ratio=0.5),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='Pad', size_divisor=32),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels']),
+]
+test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(
+        type='MultiScaleFlipAug',
+        img_scale=(1024, 1024),
+        flip=False,
+        transforms=[
+            dict(type='Resize', keep_ratio=True),
+            dict(type='RandomFlip'),
+            dict(type='Normalize', **img_norm_cfg),
+            dict(type='Pad', size_divisor=32),
+            dict(type='ImageToTensor', keys=['img']),
+            dict(type='Collect', keys=['img']),
+        ])
+]
+data = dict(
+    samples_per_gpu=8, # batch size
+    workers_per_gpu=4,
+    train=dict(
+        type=dataset_type,
+        classes=classes,
+        ann_file=data_root + 'kfold/cv_train_3.json',
+        img_prefix=data_root,
+        pipeline=train_pipeline),
+    val=dict(
+        type=dataset_type,
+        classes=classes,
+        ann_file=data_root + 'kfold/cv_val_3.json',
+        img_prefix=data_root,
+        pipeline=test_pipeline),
+    test=dict(
+        type=dataset_type,
+        classes=classes,
+        ann_file=data_root + 'test.json',
+        img_prefix=data_root,
+        pipeline=test_pipeline))
+evaluation = dict(interval=1, metric='bbox', classwise=True)
---
+++ seokyong/mmdetection/_boost_/_base_/default_runtime.py
@@ -0,0 +1,31 @@
+checkpoint_config = dict(max_keep_ckpts=3, interval=1) # dict(interval=1)
+# yapf:disable
+log_config = dict(
+    interval=100,
+    hooks=[
+        dict(type='TextLoggerHook', interval=100),
+        dict(type='WandbLoggerHook',interval=100,
+            init_kwargs=dict(
+                project='two-stage-model',
+                entity = 'canvas11',
+                name = 'HEO-htc-swins-fp16-1024-aug_lee'
+            ),
+        )
+        # dict(type='TensorboardLoggerHook')
+    ])
+# yapf:enable
+custom_hooks = [dict(type='NumClassCheckHook')]
+
+dist_params = dict(backend='nccl')
+log_level = 'INFO'
+load_from = None
+resume_from = None
+workflow = [('train', 1)]
+seed = 2022
+gpu_ids = [0]
+work_dir = './work_dirs/htc_swins_fp16_1024_aug_fpn_trash'
+
+# disable opencv multithreading to avoid system being overloaded
+opencv_num_threads = 0
+# set multi-process start method as `fork` to speed up the training
+mp_start_method = 'fork'
