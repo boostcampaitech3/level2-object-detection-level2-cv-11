4641171e1f6ff44faf9ba1920558093c7a111a6d
HeoSeokYong <67945696+HeoSeokYong@users.noreply.github.com>
2022-04-03 21:21:01 +0900

    Upload TTA seokyong

---
+++ seokyong/mmdetection/_boost_/_base_/datasets/coco_detection_aug_tta.py
@@ -0,0 +1,118 @@
+# dataset settings
+dataset_type = 'CocoDataset'
+data_root = '/opt/ml/detection/dataset/'
+
+classes = ("General trash", "Paper", "Paper pack", "Metal", "Glass", 
+           "Plastic", "Styrofoam", "Plastic bag", "Battery", "Clothing")
+
+img_norm_cfg = dict(
+    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+
+img_scale = (1024, 1024)
+
+albu_train_transforms = [
+    dict(
+    type='OneOf',
+    transforms=[
+        dict(type='Flip',p=1.0),
+        dict(type='RandomRotate90',p=1.0)
+    ],
+    p=0.5),
+    dict(type='RandomResizedCrop',height=img_scale[0], width=img_scale[1], scale=(0.5, 1.0), p=0.5),
+    dict(type='RandomBrightnessContrast',brightness_limit=0.1, contrast_limit=0.15, p=0.5),
+    dict(type='HueSaturationValue', hue_shift_limit=15, sat_shift_limit=25, val_shift_limit=10, p=0.5),
+    dict(type='GaussNoise', p=0.3),
+    dict(
+    type='OneOf',
+    transforms=[
+        dict(type='Blur', p=1.0),
+        dict(type='GaussianBlur', p=1.0),
+        dict(type='MedianBlur', blur_limit=5, p=1.0),
+        dict(type='MotionBlur', p=1.0)
+    ],
+    p=0.1)
+]
+
+train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='LoadAnnotations', with_bbox=True),
+    dict(type='Resize', img_scale=img_scale, keep_ratio=True),
+    dict(type='RandomFlip', flip_ratio=0.0), #### => 없애면 에러나는듯 https://github.com/open-mmlab/mmdetection/issues/3359
+    dict(
+        type='Albu',
+        transforms=albu_train_transforms,
+        bbox_params=dict(
+            type='BboxParams',
+            format='pascal_voc',
+            label_fields=['gt_labels'],
+            min_visibility=0.0,
+            filter_lost_elements=True),
+        keymap={
+            'img': 'image',
+            # 'gt_masks': 'masks',
+            'gt_bboxes': 'bboxes'
+        },
+        update_pad_shape=False,
+        skip_img_without_anno=True),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='Pad', size_divisor=32),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels']),
+]
+
+val_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(
+        type='MultiScaleFlipAug',
+        img_scale=img_scale,
+        flip=False,
+        transforms=[
+            dict(type='Resize', keep_ratio=True),
+            dict(type='RandomFlip'),
+            dict(type='Normalize', **img_norm_cfg),
+            dict(type='Pad', size_divisor=32),
+            dict(type='ImageToTensor', keys=['img']),
+            dict(type='Collect', keys=['img']),
+        ])
+]
+
+tta_img_scale = [(1024, 1024), (1024, 768), (768, 1024), (768, 768)]
+
+test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(
+        type='MultiScaleFlipAug',
+        img_scale=tta_img_scale,
+        flip=True,
+        transforms=[
+            dict(type='Resize', keep_ratio=True),
+            dict(type='RandomFlip'),
+            dict(type='Normalize', **img_norm_cfg),
+            dict(type='Pad', size_divisor=32), ## 한번 빼 보기
+            dict(type='ImageToTensor', keys=['img']),
+            dict(type='Collect', keys=['img']),
+        ])
+]
+
+data = dict(
+    samples_per_gpu=8,
+    workers_per_gpu=4,
+    train=dict(
+        type=dataset_type,
+        classes = classes, ###########
+        ann_file=data_root + 'kfold/cv_train_5.json', ###########
+        img_prefix=data_root,  ###########
+        pipeline=train_pipeline),
+    val=dict(
+        type=dataset_type,
+        classes = classes, ###########
+        ann_file=data_root + 'kfold/cv_val_5.json', ###########
+        img_prefix=data_root, ###########
+        pipeline=val_pipeline),
+    test=dict(
+        type=dataset_type,
+        classes = classes, ###########
+        ann_file=data_root + 'test.json', ###########
+        img_prefix=data_root, ###########
+        pipeline=test_pipeline))
+evaluation = dict(interval=1, metric='bbox', classwise=True)
